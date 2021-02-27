8884c21035728ba1ffc3e8efc7ff5bae858c9c56
Matthew Yong <matthewyfy@gmail.com>
2021-02-28 10:48:18 +1100

    new index.tsx and app component

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,14 +1,53 @@
-import React from 'react';
-import './App.css';
+import React, { Fragment } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Test
-      </header>
-    </div>
-  );
-}
+import "fontsource-roboto";
+import { Container, GridList, GridListTile } from "@material-ui/core";
 
-export default App;
+import { WelcomeScreen } from "./components/WelcomeScreen";
+import { DataTile, MqttDataType } from "./components/DataTile";
+
+const tileData: MqttDataType[] = [
+  {
+    key: 1,
+    type: "Temperature",
+    state: "10 Degrees",
+    date: "27/02/2021",
+    time: "12:02 p.m.",
+  },
+  {
+    key: 2,
+    type: "Humidity",
+    state: "30%",
+    date: "27/02/2021",
+    time: "12:02 p.m.",
+  },
+  {
+    key: 3,
+    type: "Led Light",
+    state: "Off",
+    date: "27/02/2021",
+    time: "12:02 p.m.",
+  },
+  {
+    key: 4,
+    type: "Button",
+    state: "Off",
+    date: "27/02/2021",
+    time: "12:02 p.m.",
+  },
+];
+
+export const App = () => (
+  <Fragment>
+    <WelcomeScreen />
+    <Container>
+      <GridList cols={2}>
+        {tileData.map((tile) => (
+          <GridListTile key={tile.key} cols={1}>
+            <DataTile {...tile} />
+          </GridListTile>
+        ))}
+      </GridList>
+    </Container>
+  </Fragment>
+);
