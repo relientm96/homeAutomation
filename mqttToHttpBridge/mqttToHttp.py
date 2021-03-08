76c6b41a2318994a21df71fd16a1de3a5d9481d2
Matthew Yong <myong@seek.com.au>
2021-03-08 14:00:41 +1100

    Adding new scripts for pi startup

--- mqttToHttpBridge/mqttToHttp.py
+++ mqttToHttpBridge/mqttToHttp.py
@@ -1,23 +1,29 @@
 import paho.mqtt.client as mqtt
 from urllib import request, parse
 
+
 def sendHttpRequest(data):
-  # Send a post request to home automation server
-  data = parse.urlencode(data).encode()
-  req = request.Request("https://testingmqtt.free.beeceptor.com", data = data)
-  resp = request.urlopen(req)
+    # Send a post request to home automation server
+    data = parse.urlencode(data).encode()
+    req = request.Request("http://localhost:10131/", data=data)
+    resp = request.urlopen(req)
 
 # The callback for when the client receives a CONNACK response from the server.
+
+
 def on_connect(client, userdata, flags, rc):
-  print("Connected with result code " + str(rc))
-  client.subscribe("#")
+    print("Connected with result code " + str(rc))
+    client.subscribe("#")
 
 # The callback for when a PUBLISH message is received from the server.
+
+
 def on_message(client, userdata, msg):
-  print(msg.topic+" "+str(msg.payload))
-  # Upon receiving message, we want to post a message to our local home automation HTTP server
-  data = { "message": msg.payload.decode("utf-8") }
-  sendHttpRequest(data)
+    print(msg.topic+" "+str(msg.payload))
+    # Upon receiving message, we want to post a message to our local home automation HTTP server
+    data = {"message": msg.payload.decode("utf-8")}
+    sendHttpRequest(data)
+
 
 client = mqtt.Client()
 client.on_connect = on_connect
@@ -30,5 +36,3 @@ client.connect("localhost", 1883, 60)
 # Other loop*() functions are available that give a threaded interface and a
 # manual interface.
 client.loop_forever()
-
-
