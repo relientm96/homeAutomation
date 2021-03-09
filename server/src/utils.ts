30ad1809246b566b173a078a9ba8022f356d1a53
Matthew Yong <matthewyfy@gmail.com>
2021-03-09 21:05:39 +1100

    Removing console logs

--- server/src/utils.ts
+++ server/src/utils.ts
@@ -6,7 +6,6 @@ export interface MqttData {
 }
 
 const _dataFilePath = "/home/pi/Documents/homeAutomationData/data.json";
-//const _dataFilePath = "./data/data.json";
 
 const _writeFileData = async (toWriteData: MqttData[]) => {
   fs.writeFile("data_tmp.json", JSON.stringify(toWriteData), (err) => {
@@ -19,19 +18,13 @@ const _writeFileData = async (toWriteData: MqttData[]) => {
 };
 
 export const readFileData = (): MqttData[] => {
-  console.log("Attempting to read file from FUNCTION");
   const data = fs.readFileSync(_dataFilePath).toString();
   const parsedData = JSON.parse(data);
-  console.log("Finished reading data from FUNCTION");
   return parsedData;
 };
 
 export const putToFile = (incomingData: MqttData) => {
-  console.log("Attempting to read file data");
   const currentData = readFileData();
-  console.log("Finished reading file data", currentData);
   const newDataList = [...currentData, incomingData];
-  console.log("Attempting to write new data");
   _writeFileData(newDataList);
-  console.log("Finished request");
 };
